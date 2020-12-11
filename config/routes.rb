41ec7ae9942076821238751d934967eea4987941
emi-sqbt <71768074+emi-sqbt@users.noreply.github.com>
2020-12-11 11:22:01 +0900

    マイページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'items/index'
+  get 'users/show'
   devise_for :users
   root 'items#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:show, :update]
 end
